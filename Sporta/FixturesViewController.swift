af6e627fea05d86ec6dd33caf8c67bbde56296f7
Mohamed Ayman <2moo.ayman@gmail.com>
2026-05-06 00:07:29 +0300

    Create initial ViewControllers

---
+++ Sporta/FixturesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FixturesViewController.swift
+//  Sporta
+//
+//  Created by Mohamed Ayman on 05/05/2026.
+//
+
+import UIKit
+
+class FixturesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sporta/TeamDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TeamDetailsViewController.swift
+//  Sporta
+//
+//  Created by Mohamed Ayman on 05/05/2026.
+//
+
+import UIKit
+
+class TeamDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
