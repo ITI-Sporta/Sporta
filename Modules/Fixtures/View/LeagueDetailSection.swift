9957fc24e7feeeb9c23c6f1f59e25910afa8b4f9
Mohamed Ayman <2moo.ayman@gmail.com>
2026-05-11 20:20:40 +0300

    Fix: add missing files to repository

---
+++ Modules/Fixtures/View/LeagueDetailSection.swift
@@ -0,0 +1,15 @@
+//
+//  LegueDetailsSection.swift
+//  Sporta
+//
+//  Created by Mohamed Ayman on 10/05/2026.
+//
+
+import Foundation
+
+enum LeagueDetailSection: Int, CaseIterable {
+    case header   = 0
+    case segment  = 1
+    case teams    = 2
+    case fixtures = 3
+}
---
+++ Modules/Fixtures/View/TeamsHeaderView.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  Sporta
+//
+//  Created by Mohamed Ayman on 10/05/2026.
+//
+
+import UIKit
+
+class TeamsHeaderView: UICollectionReusableView {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text      = "Teams"
+        label.font      = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = UIColor(red: 0.078, green: 0.156, blue: 0.313, alpha: 1.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+        addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 6),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
