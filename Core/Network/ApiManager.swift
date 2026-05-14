e7a443cc4481c4b0815461adad5cb6d26e3f062a
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2026-05-14 11:29:46 +0300

    Added api manager tests

--- Core/Network/ApiManager.swift
+++ Core/Network/ApiManager.swift
@@ -30,17 +30,6 @@ protocol ApiManager {
         teamId: Int,
         completion: @escaping (Result<[Fixture], AllSportsError>) -> Void
     )
-    func fetchStandings(
-        for sport: Sport,
-        leagueId: Int,
-        completion: @escaping (Result<StandingsResult, AllSportsError>) -> Void
-    )
-    func fetchH2H(
-        for sport: Sport,
-        firstTeamId: Int,
-        secondTeamId: Int,
-        completion: @escaping (Result<H2HResult, AllSportsError>) -> Void
-    )
     func fetchTeamDetails(
         for sport: Sport,
         teamId: Int,
